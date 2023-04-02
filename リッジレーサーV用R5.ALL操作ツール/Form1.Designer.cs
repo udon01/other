322d8f23bd77c86e6f5e4bf6e64c5f126ed54d89
うどん01 <104125960+udon01@users.noreply.github.com>
2023-04-03 01:46:27 +0900

    Add files via upload

---
+++ リッジレーサーV用R5.ALL操作ツール/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace リッジレーサーV用R5.ALL操作ツール
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label1 = new System.Windows.Forms.Label();
+            this.ProgressBar1 = new System.Windows.Forms.ProgressBar();
+            this.BackgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioPAL = new System.Windows.Forms.RadioButton();
+            this.radioUS = new System.Windows.Forms.RadioButton();
+            this.radioJP = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(10, 38);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(191, 12);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "exeに直接ドラッグアンドドロップしてね！";
+            // 
+            // ProgressBar1
+            // 
+            this.ProgressBar1.Location = new System.Drawing.Point(12, 12);
+            this.ProgressBar1.Name = "ProgressBar1";
+            this.ProgressBar1.Size = new System.Drawing.Size(300, 18);
+            this.ProgressBar1.TabIndex = 1;
+            // 
+            // BackgroundWorker1
+            // 
+            this.BackgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            this.BackgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorker1_ProgressChanged);
+            this.BackgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker1_RunWorkerCompleted);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "または...設定";
+            // 
+            // radioPAL
+            // 
+            this.radioPAL.AutoSize = true;
+            this.radioPAL.Checked = global::リッジレーサーV用R5.ALL操作ツール.Properties.Settings.Default.PALchecked;
+            this.radioPAL.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::リッジレーサーV用R5.ALL操作ツール.Properties.Settings.Default, "PALchecked", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.radioPAL.Location = new System.Drawing.Point(104, 85);
+            this.radioPAL.Name = "radioPAL";
+            this.radioPAL.Size = new System.Drawing.Size(44, 16);
+            this.radioPAL.TabIndex = 5;
+            this.radioPAL.Text = "PAL";
+            this.radioPAL.UseVisualStyleBackColor = true;
+            this.radioPAL.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioPAL_MouseClick);
+            // 
+            // radioUS
+            // 
+            this.radioUS.AutoSize = true;
+            this.radioUS.Checked = global::リッジレーサーV用R5.ALL操作ツール.Properties.Settings.Default.USchecked;
+            this.radioUS.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::リッジレーサーV用R5.ALL操作ツール.Properties.Settings.Default, "USchecked", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.radioUS.Location = new System.Drawing.Point(58, 85);
+            this.radioUS.Name = "radioUS";
+            this.radioUS.Size = new System.Drawing.Size(38, 16);
+            this.radioUS.TabIndex = 4;
+            this.radioUS.Text = "US";
+            this.radioUS.UseVisualStyleBackColor = true;
+            this.radioUS.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioUS_MouseClick);
+            // 
+            // radioJP
+            // 
+            this.radioJP.AutoSize = true;
+            this.radioJP.Checked = global::リッジレーサーV用R5.ALL操作ツール.Properties.Settings.Default.JPchecked;
+            this.radioJP.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::リッジレーサーV用R5.ALL操作ツール.Properties.Settings.Default, "JPchecked", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.radioJP.Location = new System.Drawing.Point(12, 85);
+            this.radioJP.Name = "radioJP";
+            this.radioJP.Size = new System.Drawing.Size(37, 16);
+            this.radioJP.TabIndex = 3;
+            this.radioJP.Text = "JP";
+            this.radioJP.UseVisualStyleBackColor = true;
+            this.radioJP.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioJP_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 110);
+            this.Controls.Add(this.radioPAL);
+            this.Controls.Add(this.radioUS);
+            this.Controls.Add(this.radioJP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ProgressBar1);
+            this.Controls.Add(this.Label1);
+            this.Name = "Form1";
+            this.Text = "リッジレーサーV用R5.ALL操作ツール";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.ProgressBar ProgressBar1;
+        private System.ComponentModel.BackgroundWorker BackgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioJP;
+        private System.Windows.Forms.RadioButton radioUS;
+        private System.Windows.Forms.RadioButton radioPAL;
+    }
+}
+
---
+++ リッジレーサーV用R5.ALL操作ツール/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace リッジレーサーV用R5.ALL操作ツール
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static int a = 0;
+        public static int filecount = 0;
+        public static bool close = false;
+        public static byte[] dainyu_none = new byte[4] { 0x00, 0x00, 0x00, 0x00 };
+        public static string JP = "SLPS_200.01";
+        public static string US = "SLUS_200.02";
+        public static string PAL = "SCES_500.00";
+        public static string JP_f = @"\SLPS_200.01";
+        public static string US_f = @"\SLUS_200.02";
+        public static string PAL_f = @"\SCES_500.00";
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            ProgressBar1.Minimum = 0;
+            ProgressBar1.Value = 0;
+            BackgroundWorker1.WorkerReportsProgress = true;
+            BackgroundWorker1.RunWorkerAsync();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        private void radioJP_MouseClick(object sender, MouseEventArgs e)
+        {
+            radioJP.Checked = true;
+            radioUS.Checked = false;
+            radioPAL.Checked = false;
+        }
+
+        private void radioUS_MouseClick(object sender, MouseEventArgs e)
+        {
+            radioJP.Checked = false;
+            radioUS.Checked = true;
+            radioPAL.Checked = false;
+        }
+
+        private void radioPAL_MouseClick(object sender, MouseEventArgs e)
+        {
+            radioJP.Checked = false;
+            radioUS.Checked = false;
+            radioPAL.Checked = true;
+        }
+
+        private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ProgressBar1.Value = a;
+            ProgressBar1.Maximum = filecount;
+            Label1.Text = a.ToString() + "/" + filecount.ToString();
+        }
+
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (close == true)
+                Close();
+        }
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgWorker = (BackgroundWorker)sender;
+            string[] path = Environment.GetCommandLineArgs();
+            if (path.Count() != 1)
+            {
+                Label1.Text = "実行中...";
+                close = true;
+            }
+
+            for (int b = 0; b < path.Count(); b++)
+            {
+                if (b == 0)
+                    goto labelfinish;
+                radioJP.Enabled = false;
+                radioUS.Enabled = false;
+                radioPAL.Enabled = false;
+                string region = "";
+                string region_f = "";
+                int writefile_count = 0;
+                if (radioJP.Checked == true)
+                {
+                    region = JP;
+                    region_f = JP_f;
+                    writefile_count = 1136;
+                }
+                else if (radioUS.Checked == true)
+                {
+                    region = US;
+                    region_f = US_f;
+                    writefile_count = 1136;
+                }
+                else
+                {
+                    region = PAL;
+                    region_f = PAL_f;
+                    writefile_count = 1208;
+                }
+                string filedir = Path.GetDirectoryName(path[b]);
+                if (File.Exists(filedir + region_f) == false)
+                    goto labelerror;
+                string fileextension = Path.GetExtension(path[b]);
+                if (fileextension == ".all" || fileextension == ".ALL")
+                    goto labelfile;
+                /*
+                else if (Directory.Exists(path[b]))
+                    goto labelfolder;
+                */
+                else
+                    goto labelerror;
+
+                labelfile:;
+                
+                // ドラッグ＆ドロップされたファイル
+                FileStream fsr = new FileStream(path[b], FileMode.Open, FileAccess.Read);
+                DirectoryInfo di = Directory.CreateDirectory(Path.GetDirectoryName(path[b]) + @"\" + Path.GetFileNameWithoutExtension(path[b]));
+                
+                FileStream fsr_toc = new FileStream(filedir + region_f, FileMode.Open, FileAccess.Read);
+                byte[] bs_toc = null;
+                if (region == JP)
+                {
+                    bs_toc = new byte[18176];
+                    fsr_toc.Seek(1097700, SeekOrigin.Begin);
+                    fsr_toc.Read(bs_toc, 0, bs_toc.Length);
+                }
+                if (region == US)
+                {
+                    bs_toc = new byte[18176];
+                    fsr_toc.Seek(1102420, SeekOrigin.Begin);
+                    fsr_toc.Read(bs_toc, 0, bs_toc.Length);
+                }
+                if (region == PAL)
+                {
+                    bs_toc = new byte[19328];
+                    fsr_toc.Seek(1115060, SeekOrigin.Begin);
+                    fsr_toc.Read(bs_toc, 0, bs_toc.Length);
+                }
+                fsr_toc.Close();
+
+                int writefile_start_seek = 4;
+                int writefile_length_seek = 12;
+                
+                for (a = 0; a < writefile_count; a++)
+                {
+                    // ファイルの開始位置
+                    int writefile_startlength = Getbyteint(bs_toc, writefile_start_seek) * 2048;
+
+                    // ファイルの長さ
+                    int writefile_length = Getbyteint(bs_toc, writefile_length_seek);
+
+                    // ファイルを書き出す
+                    byte[] writefile_file = new byte[writefile_length];
+                    fsr.Seek(writefile_startlength, SeekOrigin.Begin);
+                    fsr.Read(writefile_file, 0, writefile_length);
+                    FileStream fsw = new FileStream(di + @"\" + string.Format("{0:00000000}", a), FileMode.Create, FileAccess.Write);
+                    fsw.Write(writefile_file, 0, writefile_file.Length);
+                    fsw.Close();
+
+                    writefile_start_seek += 16;
+                    writefile_length_seek += 16;
+                }
+                FileStream fsw_toc = new FileStream(Path.GetDirectoryName(path[b]) + @"\" + "R5.TOC", FileMode.Create, FileAccess.Write);
+                fsw_toc.Write(bs_toc, 0, bs_toc.Length);
+                fsw_toc.Close();
+                fsr.Close();
+                goto labelfinish;
+
+
+            //labelfolder:;
+
+            labelerror:;
+                if (File.Exists(filedir + region_f) == false)
+                    MessageBox.Show("R5.ALLと同じディレクトリに" + region + "を置いてください" + "\r\n" +
+                                    "Put" + region + "in the same directory as R5.ALL");
+                string fileextensionf = Path.GetExtension(path[b]);
+                if (fileextensionf != ".all" && fileextensionf != ".ALL")
+                    MessageBox.Show("このファイルはR5.ALLではありません" + "\r\n" + "This file is not R5.ALL");
+
+            labelfinish:;
+            }
+        }
+        
+        //byte配列4バイトをintに変換して戻す
+        public static int Getbyteint(byte[] bytes, int seek)
+        {
+            byte[] byte1 = new byte[1];
+            Array.Copy(bytes, seek, byte1, 0, 1);
+            byte[] byte2 = new byte[1];
+            Array.Copy(bytes, seek + 1, byte2, 0, 1);
+            byte[] byte3 = new byte[1];
+            Array.Copy(bytes, seek + 2, byte3, 0, 1);
+            byte[] byte4 = new byte[1];
+            Array.Copy(bytes, seek + 3, byte4, 0, 1);
+
+            string str1 = BitConverter.ToString(byte1);
+            string str2 = BitConverter.ToString(byte2);
+            string str3 = BitConverter.ToString(byte3);
+            string str4 = BitConverter.ToString(byte4);
+            int bytelength = 0;
+
+            if (str4 != "00")
+            {
+                bytelength = 4;
+                goto label_byteget;
+            }
+            else if (str3 != "00")
+            {
+                bytelength = 3;
+                goto label_byteget;
+            }
+            else if (str2 != "00")
+            {
+                bytelength = 2;
+                goto label_byteget;
+            }
+            else if (str1 != "00")
+            {
+                bytelength = 1;
+                goto label_byteget;
+            }
+
+            else
+                return 0;
+
+            label_byteget:;
+
+            string str16 = "";
+            if (bytelength == 1)
+                str16 = str1;
+            else if (bytelength == 2)
+                str16 = str2 + str1;
+            else if (bytelength == 3)
+                str16 = str3 + str2 + str1;
+            else if (bytelength == 4)
+                str16 = str4 + str3 + str2 + str1;
+
+            int returnint = Convert.ToInt32(str16, 16);
+
+            return returnint;
+        }
+
+        //intをbyte配列4バイト(リトルエンディアン)に変換して戻す
+        public static byte[] Gethex(int hex)
+        {
+            string hexstr = hex.ToString("X");
+            if (hexstr.Length == 1 || hexstr.Length == 3 || hexstr.Length == 5 || hexstr.Length == 7)
+                hexstr = "0" + hexstr;
+
+            if (hexstr.Length == 2)
+                hexstr = hexstr + "000000";
+
+            else if (hexstr.Length == 4)
+            {
+                string hexstr1 = hexstr.Substring(0, 2);
+                string hexstr2 = hexstr.Substring(2, 2);
+                hexstr = hexstr2 + hexstr1 + "0000";
+            }
+
+            else if (hexstr.Length == 6)
+            {
+                string hexstr1 = hexstr.Substring(0, 2);
+                string hexstr2 = hexstr.Substring(2, 2);
+                string hexstr3 = hexstr.Substring(4, 2);
+                hexstr = hexstr3 + hexstr2 + hexstr1 + "00";
+            }
+
+            else if (hexstr.Length == 8)
+            {
+                string hexstr1 = hexstr.Substring(0, 2);
+                string hexstr2 = hexstr.Substring(2, 2);
+                string hexstr3 = hexstr.Substring(4, 2);
+                string hexstr4 = hexstr.Substring(6, 2);
+                hexstr = hexstr4 + hexstr3 + hexstr2 + hexstr1;
+            }
+            byte[] hexbyte = new byte[4];
+            hexbyte = StringToBytes(hexstr);
+            return hexbyte;
+        }
+
+        // 16進数文字列 => Byte配列
+        public static byte[] StringToBytes(string str)
+        {
+            var bs = new List<byte>();
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                bs.Add(Convert.ToByte(str.Substring(i * 2, 2), 16));
+            }
+            return bs.ToArray();
+        }
+    }
+}
